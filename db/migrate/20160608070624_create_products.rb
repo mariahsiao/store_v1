5d9380b63bdb0856ee5a41403f20f7ec0c1365fd
mariahsiao <thatsjustmeiwant@gmail.com>
2016-06-08 15:26:17 +0800

    create admin
    create admin/products

---
+++ db/migrate/20160608070624_create_products.rb
@@ -0,0 +1,13 @@
+class CreateProducts < ActiveRecord::Migration
+  def change
+    create_table :products do |t|
+      t.string :title
+      t.text :description
+      t.integer :quantity
+      t.string :price
+      t.string :integer
+
+      t.timestamps null: false
+    end
+  end
+end
