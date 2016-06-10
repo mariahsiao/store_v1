3d7e944e7b0caf0a37b2c5d660abff0b6df6a0b2
mariahsiao <thatsjustmeiwant@gmail.com>
2016-06-10 21:18:09 +0800

    creat order and shipping information

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   root "products#index"
 
-  resources :carts
+  resources :carts do
+    collection do
+      post :checkout
+    end
+  end
 
   devise_for :users
   namespace :admin do
