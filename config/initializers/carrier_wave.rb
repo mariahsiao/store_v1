40a2bffd508e7495e1d3dc245a6ff7abe45770b7
mariahsiao <thatsjustmeiwant@gmail.com>
2016-06-16 00:44:15 +0800

    fix bug

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -2,9 +2,9 @@ CarrierWave.configure do |config|
   if Rails.env.production?
     config.storage :fog
     config.fog_credentials = {
-      provider:              'AWS'
-      aws_access_key_id:     ENV['S3_key_id']
-      aws_secret_access_key: ENV['S3_key_secret']
+      provider:              'AWS',
+      aws_access_key_id:     ENV['S3_key_id'],
+      aws_secret_access_key: ENV['S3_key_secret'],
       region:                'ap-northeast-1'
     }
     config.fog_directory = 'maria-artstore'
